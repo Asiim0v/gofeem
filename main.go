a216aa7fa0faf270a76b73a69be25edb27d81d80
Asiim0v <664251936@qq.com>
2022-03-23 21:07:07 +0800

    add static route

--- main.go
+++ main.go
@@ -1,28 +1,50 @@
 package main
 
 import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/zserge/lorca"
 )
+
+//go:embed frontend/dist/*
+var FS embed.FS
 
 func main() {
 	// Gin 协程
 	go func() {
 		gin.SetMode(gin.DebugMode)
 		router := gin.Default()
-		router.GET("/ping", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "pong",
-			})
+		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.StaticFS("/static", http.FS(staticFiles))
+		router.NoRoute(func(c *gin.Context) {
+			path := c.Request.URL.Path
+			if strings.HasPrefix(path, "/static/") {
+				reader, err := staticFiles.Open("index.html")
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer reader.Close()
+				stat, err := reader.Stat()
+				if err != nil {
+					log.Fatal(err)
+				}
+				c.DataFromReader(http.StatusOK, stat.Size(), "text/html", reader, nil)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
 		})
 		router.Run(":8080")
 	}()
 
 	var ui lorca.UI
-	ui, _ = lorca.New("http://localhost:8080/ping", "", 800, 600)
+	ui, _ = lorca.New("http://localhost:8080/static/index.html", "", 800, 600)
 
 	chSignal := make(chan os.Signal, 1)
 	signal.Notify(chSignal, os.Interrupt)
